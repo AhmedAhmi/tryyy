e7a8b423f543a829f5a86a40c08d42cd49562121
AhmedAhmi <162032116+AhmedAhmi@users.noreply.github.com>
2024-05-14 18:22:37 +0400

    untitled-app (1) initial version

---
+++ src/app/master-view/master-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IgxButtonDirective, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective } from 'igniteui-angular';
+
+@Component({
+  selector: 'app-master-view',
+  standalone: true,
+  imports: [IgxToggleActionDirective, IgxOverlayOutletDirective, IgxButtonDirective, IgxRippleDirective, IgxToggleDirective, RouterLink],
+  templateUrl: './master-view.component.html',
+  styleUrls: ['./master-view.component.scss']
+})
+export class MasterViewComponent {}
